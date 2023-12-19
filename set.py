2596748023b1e6d03a9b3c8ed5e66743842a1a9e
Thulasi kumar <131241186+ThulasiK7@users.noreply.github.com>
2023-12-19 10:16:38 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,26 @@
+#set
+"""collection of objects
+heterogenious
+represented using { }
+index not allowed
+slicing not allowed
+duplicates not alowed
+"""
+s={1,2,3,4}
+print(s,type(s))
+s.add(10)
+print(s)
+s.remove(1)
+print(s)
+
+#how to create empty list
+a={}
+print(a,type(a))
+
+a=set()
+print(a,type(a))
+
+#create set with one element
+b={10}
+print(b,type(b))
+
---
+++ tuple.py
@@ -0,0 +1,8 @@
+#tuple
+
+#create tuple with one element
+t=(11)
+print(t,type(t))
+
+t1=(11,)
+print(t1,type(t1))
