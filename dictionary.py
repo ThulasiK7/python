da8d7c0b7172408c3022bfb41712e51c0f08b26f
Thulasi kumar <131241186+ThulasiK7@users.noreply.github.com>
2023-12-19 10:29:32 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,16 @@
+""" dictionary
+collection of objects 
+index is not allowed
+mutable
+collection of key&values
+keys will act as index
+duplicate values are allowed
+both key&value can be heterogenious i.e.any type of data
+
+"""
+
+d={1:"one",2:"Two",3:"three",1:"four"}
+print(d,type(d))
+print(d[1])
+a1={1:'a','a':"one",1.2:3}
+print(a1,type(a1))
